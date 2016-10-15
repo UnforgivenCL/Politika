c7011502bc49aac69fc2a359136e72b2a464559e
UnforgivenCL <guillermo.lobos02@inacapmail.cl>
2016-10-15 20:46:54 -0300

    Add jumbotron with content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="jumbotron">
+	<div class="jumbotron" id="landing">
 		<div class="container">
 			<h1>Politika</h1>
-			<h4>Porque nuestro deseo, es promover el acceso a ella. ¿Que esperas para unirte y debatir?</h4>
+			<h3>Porque nuestro deseo, es promover el acceso a ella. ¿Que esperas para unirte y debatir?</h3>
 			<button class="btn btn-lg btn-info">Quiero Unirme</button>
 		</div>
 	</div>
